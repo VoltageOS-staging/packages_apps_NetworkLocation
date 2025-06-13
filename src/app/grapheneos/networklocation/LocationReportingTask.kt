1d4d55066f6039d7c30d15732cde3d18eb539558
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-06-12 21:00:14 -0700

    add PositionedCellInfo and estimateLocationCell()
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/app/grapheneos/networklocation/LocationReportingTask.kt
+++ src/app/grapheneos/networklocation/LocationReportingTask.kt
@@ -6,9 +6,17 @@ import android.location.provider.LocationProviderBase
 import android.location.provider.ProviderRequest
 import android.net.wifi.ScanResult
 import android.os.SystemClock
+import android.telephony.CellIdentityGsm
+import android.telephony.CellIdentityLte
+import android.telephony.CellIdentityNr
+import android.telephony.CellIdentityWcdma
+import android.telephony.CellInfo
 import android.util.Log
 import app.grapheneos.networklocation.cell.CellPositioningServiceCache
+import app.grapheneos.networklocation.cell.CellTowerPositioningData
 import app.grapheneos.networklocation.cell.CellTowerScanner
+import app.grapheneos.networklocation.cell.Identifier
+import app.grapheneos.networklocation.cell.Standard
 import app.grapheneos.networklocation.interop.position_estimation.Coordinate
 import app.grapheneos.networklocation.interop.position_estimation.Measurement
 import app.grapheneos.networklocation.interop.position_estimation.Position
@@ -25,6 +33,7 @@ import kotlin.math.max
 import kotlin.math.pow
 import kotlin.math.sqrt
 import kotlin.time.Duration.Companion.microseconds
+import kotlin.time.Duration.Companion.milliseconds
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
@@ -87,6 +96,11 @@ class LocationReportingTask(
         val scanResult: ScanResult,
         val positioningData: PositioningData,
         var estimatedDistance: EstimatedDistance?,
+    )
+
+    private class PositionedCellInfo(
+        val cellInfo: CellInfo,
+        val positioningData: PositioningData,
     )
 
     private fun estimateLocationWifi(scanResults: List<ScanResult>): Location? {
@@ -248,6 +262,228 @@ class LocationReportingTask(
 
         loc.elapsedRealtimeNanos =
             bestResults.values.minOf { it.scanResult.timestamp }.microseconds.inWholeNanoseconds
+        val locationAgeMillis =
+            SystemClock.elapsedRealtime() - loc.elapsedRealtimeNanos / 1_000_000L
+        loc.time = max(0L, System.currentTimeMillis() - locationAgeMillis)
+
+        val enuPoint = Point(result.x.value, result.y.value, result.z.value)
+        val estimatedGeoPoint = enuPointToGeoPoint(enuPoint, refGeoPoint)
+        loc.longitude = estimatedGeoPoint.longitude
+        loc.latitude = estimatedGeoPoint.latitude
+        loc.accuracy = ((sqrt(result.x.variance) + sqrt(result.y.variance)) / 2.0).toFloat()
+        estimatedGeoPoint.altitude?.let { estimatedAltitude ->
+            loc.altitude = estimatedAltitude
+            loc.verticalAccuracyMeters = sqrt(result.z.variance).toFloat()
+        }
+        return loc
+    }
+
+    private fun estimateLocationCell(cellInfos: List<CellInfo>): Location? {
+        val bestResults = HashMap<Identifier, PositionedCellInfo>()
+
+        val identifiers = cellInfos.mapNotNull {
+            val mcc = it.cellIdentity.mccString?.toIntOrNull() ?: return@mapNotNull null
+            val mnc = it.cellIdentity.mncString?.toIntOrNull() ?: return@mapNotNull null
+
+            val identifier = when (it.cellIdentity) {
+                is CellIdentityNr -> {
+                    val identity = it.cellIdentity as CellIdentityNr
+                    Identifier(
+                        Standard.NR,
+                        mcc,
+                        mnc,
+                        identity.tac.let { tac ->
+                            if (tac != CellInfo.UNAVAILABLE) {
+                                tac
+                            } else {
+                                return@mapNotNull null
+                            }
+                        },
+                        identity.nci.let { nci ->
+                            if (nci != CellInfo.UNAVAILABLE_LONG) {
+                                nci
+                            } else {
+                                return@mapNotNull null
+                            }
+                        }
+                    )
+                }
+
+                is CellIdentityLte -> {
+                    val identity = it.cellIdentity as CellIdentityLte
+                    Identifier(
+                        Standard.LTE,
+                        mcc,
+                        mnc,
+                        identity.tac.let { tac ->
+                            if (tac != CellInfo.UNAVAILABLE) {
+                                tac
+                            } else {
+                                return@mapNotNull null
+                            }
+                        },
+                        identity.ci.let { cellId ->
+                            if (cellId != CellInfo.UNAVAILABLE) {
+                                cellId
+                            } else {
+                                return@mapNotNull null
+                            }.toLong()
+                        }
+                    )
+                }
+
+                is CellIdentityWcdma -> {
+                    val identity = it.cellIdentity as CellIdentityWcdma
+                    Identifier(
+                        Standard.WCDMA,
+                        mcc,
+                        mnc,
+                        identity.lac.let { lac ->
+                            if (lac != CellInfo.UNAVAILABLE) {
+                                lac
+                            } else {
+                                return@mapNotNull null
+                            }
+                        },
+                        identity.cid.let { cellId ->
+                            if (cellId != CellInfo.UNAVAILABLE) {
+                                cellId
+                            } else {
+                                return@mapNotNull null
+                            }.toLong()
+                        }
+                    )
+                }
+
+                is CellIdentityGsm -> {
+                    val identity = it.cellIdentity as CellIdentityGsm
+                    Identifier(
+                        Standard.GSM,
+                        mcc,
+                        mnc,
+                        identity.lac.let { lac ->
+                            if (lac != CellInfo.UNAVAILABLE) {
+                                lac
+                            } else {
+                                return@mapNotNull null
+                            }
+                        },
+                        identity.cid.let { cellId ->
+                            if (cellId != CellInfo.UNAVAILABLE) {
+                                cellId
+                            } else {
+                                return@mapNotNull null
+                            }.toLong()
+                        }
+                    )
+                }
+
+                else -> return@mapNotNull null
+            }
+
+            Pair(identifier, it)
+        }.toMap()
+
+        if (identifiers.isEmpty()) {
+            return null
+        }
+
+        val allPositioningData = mutableListOf<CellTowerPositioningData>()
+
+        try {
+            // just in case the potential additional requests fail, add only cached ones
+            allPositioningData.addAll(
+                cellService.getPositioningData(
+                    identifiers.keys.toList(),
+                    0
+                )
+            )
+
+            // don't make additional requests when we have 8 of the closest results with valid
+            // positioning data already
+            val onlyCachedThreshold = 8
+            val result =
+                cellService.getPositioningData(identifiers.keys.toList(), onlyCachedThreshold)
+            allPositioningData.clear()
+            allPositioningData.addAll(result)
+        } catch (e: IOException) {
+            Log.d(TAG, "unable to obtain positioning data: $e")
+            if (Log.isLoggable(TAG, Log.VERBOSE)) {
+                Log.v(TAG, "", e)
+            }
+        }
+
+        for (data in allPositioningData) {
+            val cellInfo = identifiers[data.identifier]
+            if (cellInfo != null && data.positioningData != null) {
+                bestResults[data.identifier] = PositionedCellInfo(cellInfo, data.positioningData)
+            }
+        }
+
+        if (bestResults.isEmpty()) {
+            return null
+        }
+
+        // use the median coordinates of nearby cells for protection against around 50%
+        // or less of them being in a wildly incorrect location
+        val refGeoPoint = GeoPoint(
+            bestResults.values.map { it.positioningData.latitude }.median() ?: return null,
+            bestResults.values.map { it.positioningData.longitude }.median() ?: return null,
+            bestResults.values.mapNotNull { it.positioningData.altitudeMeters }.let {
+                if (it.isNotEmpty()) it.average() else null
+            }
+        )
+
+        val measurements = bestResults.values.map { result ->
+            val positioningData = result.positioningData
+            // convert position to Cartesian coordinates
+            val position = geoPointToEnuPoint(
+                GeoPoint(
+                    positioningData.latitude,
+                    positioningData.longitude,
+                    positioningData.altitudeMeters?.toDouble()
+                ),
+                refGeoPoint
+            )
+            // the accuracyMeters for cell towers is the range of where the device could be located
+            // if it can see the tower
+            val xyPositionVariance = positioningData.accuracyMeters.toDouble().pow(2)
+            val zPositionVariance = positioningData.verticalAccuracyMeters?.toDouble()?.pow(2)
+            Measurement(
+                Position(
+                    Coordinate(
+                        true,
+                        position.x,
+                        xyPositionVariance,
+                    ),
+                    Coordinate(
+                        true,
+                        position.y,
+                        xyPositionVariance,
+                    ),
+                    Coordinate(
+                        position.z != null,
+                        position.z ?: 0.0,
+                        zPositionVariance ?: 0.0,
+                    ),
+                ),
+                // we can't estimate distance for cell towers
+                0.0,
+                0.0,
+            )
+        }
+
+        val time = SystemClock.elapsedRealtime()
+        val result = PositionEstimation.main(measurements.toTypedArray())
+        verboseLog(TAG) { "estimateLocationCell took ${(SystemClock.elapsedRealtime() - time)} ms" }
+        if (result == null) {
+            return null
+        }
+
+        val loc = Location(LocationManager.NETWORK_PROVIDER)
+
+        loc.elapsedRealtimeNanos =
+            bestResults.values.minOf { it.cellInfo.timestampMillis }.milliseconds.inWholeNanoseconds
         val locationAgeMillis =
             SystemClock.elapsedRealtime() - loc.elapsedRealtimeNanos / 1_000_000L
         loc.time = max(0L, System.currentTimeMillis() - locationAgeMillis)
